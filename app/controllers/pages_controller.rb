022e43f171cc57a68b211a742e1c7681d620000d
Milan Kecman <milan.kecman270@gmail.com>
2025-02-13 13:23:14 +0100

    Add Free CSS Template

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,22 @@
+class PagesController < ApplicationController
+  def index
+  end
+
+  def about
+  end
+
+  def contact
+  end
+
+  def property_listing
+  end
+
+  def property_types
+  end
+
+  def our_agents
+  end
+
+  def testimonials
+  end
+end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,13 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import "jquery";
+import "easing";
+import "easing.min"
+import "owl.carousel"
+import "owl.carousel.min"
+import "waypoints.min"
+import "wow"
+import "wow.min"
+import "main"
+import * as bootstrap from "bootstrap"
