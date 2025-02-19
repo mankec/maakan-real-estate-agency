dc8bec52aefa89be6b836131a08c915e0722f6a7
Milan Kecman <milan.kecman270@gmail.com>
2025-02-19 14:13:03 +0100

    Enable user to add property

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   get "/contact", to: "pages#contact"
   get "/property_listing", to: "pages#property_listing"
   get "/property_types", to: "pages#property_types"
+  get "/add-property", to: "pages#add_property", as: :add_property
+  post "/update-property-form", to: "pages#update_property_form", as: :update_property_form
   get "/our-agents", to: "pages#our_agents"
   get "/testimonials", to: "pages#testimonials"
 
---
+++ lib/property_factory.rb
@@ -0,0 +1,12 @@
+class PropertyFactory
+  def self.build(property_type)
+    case property_type.to_sym
+    when :apartment
+      Apartment.new
+    when :dummy
+      Dummy.new
+    else
+      raise "Unknown property type"
+    end
+  end
+end
