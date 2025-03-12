55718846afa097070c365041951f19b1d6dedbca
Milan Kecman <milan.kecman270@gmail.com>
2025-03-12 14:24:58 +0100

    Update homepage

--- app/javascript/main.js
+++ app/javascript/main.js
@@ -12,6 +12,27 @@ document.addEventListener("turbo:load", function() {
             '<i class="bi bi-chevron-right"></i>'
         ]
     });
+
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        margin: 24,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="bi bi-arrow-left"></i>',
+            '<i class="bi bi-arrow-right"></i>'
+        ],
+        responsive: {
+            0:{
+                items:1
+            },
+            992:{
+                items:2
+            }
+        }
+    });
 });
 
 
@@ -55,44 +76,5 @@ document.addEventListener("turbo:load", function() {
         $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
         return false;
     });
-
-
-    // Header carousel
-    // $(".header-carousel").owlCarousel({
-    //     autoplay: true,
-    //     smartSpeed: 1500,
-    //     items: 1,
-    //     dots: true,
-    //     loop: true,
-    //     nav : true,
-    //     navText : [
-    //         '<i class="bi bi-chevron-left"></i>',
-    //         '<i class="bi bi-chevron-right"></i>'
-    //     ]
-    // });
-
-
-    // Testimonials carousel
-    $(".testimonial-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1000,
-        margin: 24,
-        dots: false,
-        loop: true,
-        nav : true,
-        navText : [
-            '<i class="bi bi-arrow-left"></i>',
-            '<i class="bi bi-arrow-right"></i>'
-        ],
-        responsive: {
-            0:{
-                items:1
-            },
-            992:{
-                items:2
-            }
-        }
-    });
-    
 })(jQuery);
 
